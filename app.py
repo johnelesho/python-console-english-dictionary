76390845c32d7f02f6278f162fa00f318675cb48
John Elesho <eleshojohno@gmail.com>
2020-10-22 07:22:30 +0100

    Single Commit- A Python console based English  dictionary

---
+++ app.py
@@ -0,0 +1,31 @@
+import json
+from difflib import get_close_matches
+
+dict = json.load(open("words.json", "r"))
+
+
+def getMeaning(word):
+    word = word.lower()
+    if word in dict:
+        meaning= dict[word]
+        
+        if len(meaning) > 0:
+           return "\n".join(meaning)
+    elif len(get_close_matches(word, dict.keys())) > 0:
+     
+       word2= input(f"\n'{word}' cannot be found see close matches {get_close_matches(word, dict.keys())}: ")
+       return getMeaning(word2)
+    else:
+        return f"\n'{word}' cannot be found "
+
+
+word_to_lookup = input('Enter the word: ')
+
+
+while word_to_lookup.lower() != "quit()":
+        print()
+        print(getMeaning(word_to_lookup))
+        print()
+      
+        word_to_lookup = input('Enter the word: "quit()" to quit program: ')
+        
